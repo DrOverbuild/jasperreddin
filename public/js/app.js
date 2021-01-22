cd7702fcaaf5e49710bccb3b2006937206d249be
Jasper Reddin <jasper@jasperreddin.com>
2021-01-21 21:17:29 -0600

    Added spotify and apple music links

--- public/js/app.js
+++ public/js/app.js
@@ -145,122 +145,118 @@
 			title: "Cantina Band -- Coffeehouse Lofi Remix",
 			artwork: "/img//albumartwork/cantinabandsmall.jpg",
 			ytId: "9Kob3vYQ6yQ",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/cantina-band-coffeehouse-lofi-single/1506437207",
+			spotifyUrl: "https://open.spotify.com/track/1J303X3dxiQqA1CkDQ1raP"
 		}, {
 			title: "Red Planet",
 			artwork: "/img/albumartwork/redplanetsmall.jpg",
 			ytId: "CFyarso6tno",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/red-planet-single/1495673733",
+			spotifyUrl: "https://open.spotify.com/track/3UGNUVxYE99mvSZAm1aBdU"
 		}, {
 			title: "Duel of the Keyboards",
 			artwork: "/img/albumartwork/fatessmall.jpg",
-			ytId: "ezGtx-X5MnI",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			ytId: "ezGtx-X5MnI"
 		}, {
 			title: "calm down",
 			artwork: "/img/albumartwork/serenade2small.jpg",
 			ytId: "ooJlM3GIehM",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/calm-down/1471647940?i=1471647950",
+			spotifyUrl: "https://open.spotify.com/track/1EAxLwMe2FHA0ZDgTw2QV0"
 		}, {
 			title: "high five",
 			artwork: "/img/albumartwork/serenade2small.jpg",
 			ytId: "DL5Q9h6dgAA",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/high-five/1471647940?i=1471647949",
+			spotifyUrl: "https://open.spotify.com/track/1hhf7opu7kngZeMky1QwYx"
 		}, {
 			title: "shimmer",
 			artwork: "/img/albumartwork/serenade2small.jpg",
 			ytId: "BE1H82AaW2k",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/shimmer/1471647940?i=1471647948",
+			spotifyUrl: "https://open.spotify.com/track/33nuc5WlllA94S6vkrMfmM"
 		}, {
 			title: "hint of funk",
 			artwork: "/img/albumartwork/serenade2small.jpg",
 			ytId: "DL5Q9h6dgAA",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/hint-of-funk/1471647940?i=1471647944",
+			spotifyUrl: "https://open.spotify.com/track/00yCAuO5k8tNk4QTp6VVQU"
 		}, {
 			title: "Thought",
 			artwork: "/img/albumartwork/tapessmall.jpg",
 			ytId: "002qTlNG63o",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/thought/1445544143?i=1445544144",
+			spotifyUrl: "https://open.spotify.com/track/1doJ5ibfLfVAq5wE6SlRsD"
 		}, {
 			title: "Intuition",
 			artwork: "/img/albumartwork/tapessmall.jpg",
 			ytId: "XUlfgJUzGBE",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
-		}, {
-			title: "Inspiration",
-			artwork: "/img/albumartwork/tapessmall.jpg",
-			ytId: "dD4ZTrpL7uc",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/intuition/1445544143?i=1445544145",
+			spotifyUrl: "https://open.spotify.com/track/7GWAPFztkA18NmdjPKAoOc"
 		}, {
 			title: "Infinity",
 			artwork: "/img/albumartwork/tapessmall.jpg",
 			ytId: "8c3mmBM80R4",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/infinity/1445544143?i=1445544146",
+			spotifyUrl: "https://open.spotify.com/track/3XVJ59dNESDhSnNIvyS69k"
+		}, {
+			title: "Inspiration",
+			artwork: "/img/albumartwork/tapessmall.jpg",
+			ytId: "dD4ZTrpL7uc",
+			appleMusicUrl: "https://music.apple.com/us/album/inspiration/1445544143?i=1445544147",
+			spotifyUrl: "https://open.spotify.com/track/0pTZ7oSeJwgGcXANXXKnUy"
 		}, {
 			title: "Justice",
 			artwork: "/img/albumartwork/tapessmall.jpg",
 			ytId: "dNSgThdQLi8",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/justice/1445544143?i=1445544148",
+			spotifyUrl: "https://open.spotify.com/track/4ZagHdWP9GblFvHwNOGhCQ"
 		}, {
 			title: "Time (Five Minutes of Extreme Depression)",
 			artwork: "/img/albumartwork/tapessmall.jpg",
 			ytId: "LVZsyt1nW3Q",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/time-five-minutes-of-extreme-depression/1445544143?i=1445544149",
+			spotifyUrl: "https://open.spotify.com/track/1eUDipzNvDye3p1dXBVbHC"
 		}, {
 			title: "Meaningless",
 			artwork: "/img/albumartwork/meaninglesssmall.jpg",
 			ytId: "K-pviuKzbrE",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/meaningless/1432638707?i=1432638910",
+			spotifyUrl: "https://open.spotify.com/track/1FyEPOdu3pu0vj1ZUaJ2Wj"
 		}, {
 			title: "7th",
 			artwork: "/img/albumartwork/7thsmall.jpg",
 			ytId: "PcFPQ6392wY",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/7th/1420451934?i=1420451946",
+			spotifyUrl: "https://open.spotify.com/track/1Jq1EY8WdhzEAvjypYcWKD"
 		}, {
 			title: "Rising Tide",
 			artwork: "/img/albumartwork/risingtidesmall.jpg",
 			ytId: "U_wcCRk-xfE",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/rising-tide/1353786926?i=1353786927",
+			spotifyUrl: "https://open.spotify.com/track/4KPE7dYGWc5hZFNcLf7wr5"
 		}, {
 			title: "The First Noel [Piano Cover]",
 			artwork: "/img/albumartwork/noelsmall.jpg",
-			ytId: "gnLwB56Nxvs",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			ytId: "gnLwB56Nxvs"
 		}, {
 			title: "Placid Synths",
 			artwork: "/img/albumartwork/placidsynthssmall.jpg",
 			ytId: "j9FLijbs47A",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/placid-synths/1292725285?i=1292725290",
+			spotifyUrl: "https://open.spotify.com/track/11n06FUiGJHy2NzfixoDAF"
 		}, {
 			title: "Compressed Fir3",
 			artwork: "/img/albumartwork/fir3small.jpg",
 			ytId: "jtchSrICwHg",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/compressed-fir3/1274956604?i=1274956608",
+			spotifyUrl: "https://open.spotify.com/track/4cAbEdA9RtbBicxtaDDoz5"
 		}, {
 			title: "Comic Punch",
 			artwork: "/img/albumartwork/comicpunchsmall.jpg",
 			ytId: "EnttmZBAiic",
-			appleMusicUrl: "/",
-			spotifyUrl: "/"
+			appleMusicUrl: "https://music.apple.com/us/album/comic-punch/1287396718?i=1287396723",
+			spotifyUrl: "https://open.spotify.com/track/5HxcdOSOe7eusQ5nvoy6wL"
 		}];
 
 		$scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
@@ -278,10 +274,18 @@
 			});
 		}
 
-		$scope.embedClicked = function(id) {
+		$scope.ytClicked = function(id) {
 			ytmodal.html(`<iframe id="ytplayer" type="text/html"` +
 		  			`src="https://www.youtube.com/embed/${id}?autoplay=1"` +
 					 `frameborder="0"></iframe>`);
+			ytmodal.fadeIn(100);
+		};
+
+		$scope.spotifyClicked = function(id) {
+			ytmodal.html(
+				`<iframe id="spotifyplayer" src="https://open.spotify.com/embed/track/${id}" ` +
+				`frameborder="0" allowtransparency="true" `  +
+				`allow="encrypted-media"></iframe>`);
 			ytmodal.fadeIn(100);
 		}
 
@@ -290,7 +294,7 @@
 		});
 
 		$(document).keydown(function (e) {
-			if($('#ytplayer')) {
+			if(e.keyCode == 27 && $('#ytplayer').is(":visible")) {
 				e.preventDefault();
 				$scope.hideYtPlayer();
 			}
